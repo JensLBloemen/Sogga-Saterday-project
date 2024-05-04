ee1797b67d4cde17dc9c4c82b2e78642ce047a1d
JelleSipkes <124881330+JelleSipkes@users.noreply.github.com>
2024-05-04 11:12:46 +0200

    Add files via upload
    Framework of project, player can walk, stays in middle of screen.

---
+++ player.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Player:
+
+    def __init__(self, x, y) -> None:
+        self.pos = np.array([x, y])
+        self.speed = 5
+        self.radius = 30
+
+    def move(self, direction : np.array) -> None:
+        self.pos += direction * self.speed
